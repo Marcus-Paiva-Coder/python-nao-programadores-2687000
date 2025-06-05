b4a2b45ceb2a944cf0a3a635daef090c91004d2d
Marcus-Paiva-Coder <marcuspaiva07@gmail.com>
2025-06-05 19:25:54 +0000

    Variáveis

--- exercicios/1-primeiros-passos/1-variaveis.py
+++ exercicios/1-primeiros-passos/1-variaveis.py
@@ -4,8 +4,18 @@
 # - E não podemos usar palavras reservadas do Python, como if, for, from, etc. 
 
 # Declare variáveis do tipo string
+nome = 'Marcus' \
+
+profissao = "Programador" \
+
+objetivo = '''Aprender python pra mudar de profissão'''
 
 # Declare variáveis de cada tipo numérico
+idade = 16
+salario = 650.50
+complexo = 2+5j
 
 
 # Declare uma variável do tipo booleano
+verdadeiro = True
+falso = False
